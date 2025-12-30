b10cb13a8ccd0e8018c1d3ce95bb17138e8bfcad
Philip Josolowitz <philipjosolowitz@MacBookPro-2.lan>
2025-12-30 09:13:39 +0200

    update

---
+++ Day5/inclass.py
@@ -0,0 +1,16 @@
+class Person():
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def show_details(self):
+    print("Hello my name is " + self.name)
+
+  def rename(self, new_name):
+        self.name = new_name
+
+first_person = Person("John", 36)
+first_person.show_details()
+first_person.rename("Julius")
+first_person.show_details()
+
